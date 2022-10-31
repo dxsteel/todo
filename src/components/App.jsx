5af2ac5a6040f398d30803ff96ce6041cfba5eeb
Dmytro Khomenko <dmytro.khomenko.it@gmail.com>
2022-10-31 23:03:42 +0200

    download components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import TodoList from "./TodoList";
+import AppHeader from "./AppHeader";
+import SearchPanel from "./SearchPanel";
+import ItemStatusFilter from "./ItemStatusFilter";
+
 export const App = () => {
+
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+      <SearchPanel />
+      <ItemStatusFilter />
+      </div>
+      <TodoList todos={todoData} />
     </div>
   );
 };
---
+++ src/components/SearchPanel.js
@@ -0,0 +1,11 @@
+import './SearchPanel.css'
+
+const SearchPanel = () => {
+ 
+  return (
+    <input type="text"
+      className='form-control search-input'
+      placeholder="type to search" />);
+};
+
+export default SearchPanel;
